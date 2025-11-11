71b9ff51e41c8a4efd43c2b23734acab8dabd59a
alexis <alexisschizas@hotmail.com>
2025-11-11 12:17:02 +0100

    add roi tool, search for vegetation hue peak in smart way instead of taking highest hue

--- README.md
+++ README.md
@@ -7,10 +7,35 @@ Estimate tree/shrub evolution using CV on aerial photos
 # install libs while in venv, save reqs with pip freeze > requirements.txt
 deactivate
 
-# TODO:
+
+# TODO GENERAL: 
+
+1. combine vegetation (hue) and feature (otsu thresholding) masks in intelligent order
+5. adapt and tune parameters, if possible make them dependent of image size
+1. create roi-selector tool
+2. create more robust hue detector: mix finding local maxima with expected green hue (problem to solve: detecting lake)
+3. clean-up code, have full pipeline to run on roi and output vegetation detected
+4. start investigating how to extract part of an image from qgis: 
+    -> create polygon
+    -> extract image of whats in the polygon
+5. move bombina variegata code to repo
+6. experiment with other vegetation indexes (not just hues)
+
+
+
+
+# TODO DAY 1:
 1. extract forest only -> otsu, clustering, closing.
 2. extract individual trees, combine with previous
 3. create mask for only green hues
 4. combine masks in intelligent order
 5. adapt and tune parameters, if possible make them dependent of image size
 
+# TODO DAY 2:
+1. create roi-selector tool
+2. create more robust hue detector: mix finding local maxima with expected green hue (problem to solve: detecting lake)
+3. clean-up code, have full pipeline to run on roi and output vegetation detected
+4. start investigating how to extract part of an image from qgis: 
+    -> create polygon
+    -> extract image of whats in the polygon
+5. move bombina variegata code to repo
--- detect_vegetation.py
+++ detect_vegetation.py
@@ -3,8 +3,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pyautogui
 from sklearn.cluster import DBSCAN
+from scipy.signal import find_peaks
 
-def resize_img_to_screen(img):
+def downsize_img_to_screen(img):
     """
     rescale potentially large images based on current screen size
     """
@@ -16,9 +17,68 @@ def resize_img_to_screen(img):
     if max(w_ratio, h_ratio) >= 1:
         fx = 0.9/(max(w_ratio, h_ratio)) # rescale to 0.9 * screen size
         img_resized = cv2.resize(img, None, fx=fx, fy=fx, interpolation=cv2.INTER_LINEAR)
-        return img_resized
+        return img_resized, fx
 
-    return img
+    return img, 1
+
+def upscale_image_to_target_size(img, scaling_factor):
+    """
+    upscale image to new w, h
+
+    used to upscale masks back to original image size
+    """
+    return cv2.resize(img, None, fx=scaling_factor, fy=scaling_factor, interpolation=cv2.INTER_LINEAR)
+     
+
+def select_roi(img):
+    
+    # downsize if image too big for screen
+    w, h = img.shape[:2] 
+    resized, scaling_factor = downsize_img_to_screen(img)
+
+    x,y,w,h = cv2.selectROI("select the area", resized)
+
+    if w == 0 and h == 0:
+        return img
+    
+    # roi = resized[int(y):int(y+h), int(x):int(x+w)]
+    # cv2.imshow("ROI selection", roi)
+    # cv2.waitKey(0)
+    # Convert to original image coordinates
+    x_orig = int(x * 1/scaling_factor)
+    y_orig = int(y * 1/scaling_factor)
+    w_orig = int(w * 1/scaling_factor)
+    h_orig = int(h * 1/scaling_factor)
+    
+    roi_original = img[y_orig:y_orig + h_orig, x_orig:x_orig + w_orig]
+    cv2.imshow("original size roi", roi_original)
+    cv2.waitKey(0)
+    return roi_original
+
+def extract_shadows(img):
+    """
+    Extract dark spots corresponding to shadows. smooth the shadows to filter noise
+    """
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    hue, sat, val = hsv[:,:,0], hsv[:,:,1], hsv[:,:,2]
+    hist, bins = np.histogram(val, bins=180, range=(0,100))
+    dominant_val_index = np.argmax(hist)
+    print("dominant value:", dominant_val_index)
+
+    plt.figure(figsize=(8,4))
+    plt.bar(bins[:-1], hist, width = 1.0)
+    plt.title("value histogram of image")
+    plt.xlabel("value (0 - 100)")
+    plt.show()
+
+    margin = 0
+    val_mask = val < 20
+    shadows = np.zeros_like(img)
+    shadows[val_mask] = 255
+
+    cv2.imshow("shadows:", downsize_img_to_screen(shadows))
+    cv2.waitKey()
+
 
 def extract_vegetation_mask(img):
     """
@@ -27,8 +87,28 @@ def extract_vegetation_mask(img):
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     hue, sat, val = hsv[:,:,0], hsv[:,:,1], hsv[:,:,2]
     hist, bins = np.histogram(hue, bins=180, range=(0,180))
-    dominant_hue_index = np.argmax(hist)
-    print("dominant hue:", dominant_hue_index)
+    
+    hist_float = hist.astype(np.float32)
+    hist_2d = hist_float.reshape(1, -1)   # shape = 1 x 180, horizontal
+    hist_smooth = cv2.GaussianBlur(hist_2d, (9,1), sigmaX=2)  # kernel(9,1) smooths along x-axis
+    hist = hist_smooth.reshape(-1)
+
+    height_thresh = np.max(hist)*0.2
+
+    peaks, properties = find_peaks(hist, height_thresh, distance=5)
+    peak_heights = properties['peak_heights']
+    print("Filtered peaks (bin, height):")
+    for p, h in zip(peaks, peak_heights):
+        print(p, h)
+
+    target_hue = 60     # vegetation hue
+
+    # Compute distance from each peak to target
+    distances = np.abs(peaks - target_hue)
+    closest_idx = np.argmin(distances)
+    vegetation_peak = peaks[closest_idx]
+
+    print(f"Selected vegetation peak: bin={vegetation_peak}")
 
     plt.figure(figsize=(8,4))
     plt.bar(bins[:-1], hist, width = 1.0)
@@ -36,21 +116,23 @@ def extract_vegetation_mask(img):
     plt.xlabel("hue (0 - 180)")
     plt.show()
 
-    margin = 25
-    hue_mask = (hue >= (dominant_hue_index - margin)) & (hue <= (dominant_hue_index + margin))
+    margin = 10
+    hue_mask = (hue >= (vegetation_peak - margin)) & (hue <= (vegetation_peak + margin))
+    # hue_mask = (hue >= (60 - margin)) & (hue <= (60 + margin))
     val_mask = val < 45
     mask = hue_mask | val_mask
     result = np.zeros_like(img)
     result[mask] = img[mask]
 
 
-    img_resized = resize_img_to_screen(result)
+    img_resized, _ = downsize_img_to_screen(result)
     cv2.imshow("filtered:", img_resized)
     cv2.waitKey()
 
     gray = cv2.cvtColor(result, cv2.COLOR_BGR2GRAY)
     _, mask = cv2.threshold(gray, 1, 255, cv2.THRESH_BINARY)
-    cv2.imshow("result:", resize_img_to_screen(mask))
+    mask_resized, _ = downsize_img_to_screen(mask)
+    cv2.imshow("result:", mask_resized)
     cv2.waitKey()
 
     # reduce noise with open + close operations
@@ -58,7 +140,8 @@ def extract_vegetation_mask(img):
     mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel, iterations=1)
     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (9,9))
     mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel, iterations=1)
-    cv2.imshow("after morph:", resize_img_to_screen(mask))
+    mask_resized, _ = downsize_img_to_screen(mask)
+    cv2.imshow("after morph:", mask_resized)
     cv2.waitKey()
 
     return mask
@@ -68,11 +151,13 @@ def extract_features_mask(img):
 
     green_channel = img[:,:,1]
     green_blurred = cv2.GaussianBlur(green_channel,(3,3),0)
-    cv2.imshow("gaussian blurr:", resize_img_to_screen(green_blurred))
+    resized, _ = downsize_img_to_screen(green_blurred)
+    cv2.imshow("gaussian blurr:", resized)
     cv2.waitKey()
 
-    ret3,otsu = cv2.threshold(green_blurred,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
-    cv2.imshow("otsu:", resize_img_to_screen(otsu))
+    _, otsu = cv2.threshold(green_blurred,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+    resized, _ = downsize_img_to_screen(otsu)
+    cv2.imshow("otsu:", resized)
     cv2.waitKey()
 
     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (13,13))
@@ -80,11 +165,13 @@ def extract_features_mask(img):
     detected = cv2.morphologyEx(otsu, cv2.MORPH_CLOSE, kernel)
     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (9,9))
     detected = cv2.morphologyEx(detected, cv2.MORPH_OPEN, kernel)
-    cv2.imshow("after morph:", resize_img_to_screen(detected))
+    resized, _ = downsize_img_to_screen(detected)
+    cv2.imshow("after morph:", resized)
     cv2.waitKey()
 
     # mask is 0/255 binary
-    ys, xs = np.where(resize_img_to_screen(detected) == 255)
+    resized, _ = downsize_img_to_screen(detected)
+    ys, xs = np.where(resized == 255)
     coords = np.vstack((xs, ys)).T
 
     # choose eps based on desired connectivity (pixel distance)
@@ -93,7 +180,7 @@ def extract_features_mask(img):
     labels = db.labels_
 
     # generate cluster mask
-    cluster_mask = np.zeros_like(resize_img_to_screen(detected), dtype=np.int32)
+    cluster_mask = np.zeros_like(resized, dtype=np.int32)
     cluster_mask[ys, xs] = labels + 1   # +1 because -1 is noise
     labels = cluster_mask   # shape (H, W)
     unique_labels = np.unique(labels)
@@ -125,25 +212,27 @@ def extract_features_mask(img):
     cv2.imshow("clusters", vis_color)
     cv2.waitKey(0)
 
-    detected_resized = resize_img_to_screen(detected)
+    detected_resized, _ = downsize_img_to_screen(detected)
     forest_and_isolated_trees = cv2.bitwise_or(detected_resized, forest_mask)
     cv2.imshow("forest and isolated trees", forest_and_isolated_trees)
     cv2.waitKey(0)
     
     return forest_and_isolated_trees
     
-    # cv2.imwrite("output.png", resize_img_to_screen(detected))
+    # cv2.imwrite("output.png", downsize_img_to_screen(detected))
 
 
 if __name__ == "__main__":
     
     print("reading image...")
-    img = cv2.imread("orthophotos/hongrin2.tif")
-    print("shape:", img.shape)
-    print("dtype:", img.dtype)
+    img = cv2.imread("orthophotos/lac.tif")
+    
+    img = select_roi(img)
 
+
+    # extract_shadows(img)
     vegetation_mask = extract_vegetation_mask(img)
-    vegetation_mask = resize_img_to_screen(vegetation_mask)
+    vegetation_mask, _ = downsize_img_to_screen(vegetation_mask)
 
     forest_features = extract_features_mask(img)
     print(vegetation_mask.shape, forest_features.shape)
